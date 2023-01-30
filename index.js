21668cb8ebca9eea272c54c402a9738d070a73ba
Sergey Bachinin <sbachinin@yandex.ru>
2023-01-30 10:56:45 +0530

    randomize color and thickness

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// UTILS
+
+const get_random_point = () => ([
+    Math.random() * canvas.width,
+    Math.random() * canvas.height
+])
+
+const get_rand_color = () => {
+    return 'rgb('
+        + Math.random() * 255
+        + ','
+        + Math.random() * 255
+        + ','
+        + Math.random() * 255
+        + ')'
+}
+
+//////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
 const w = 900, h = 1200
 
 
@@ -6,14 +37,8 @@ canvas.width = w + 400
 canvas.height = h + 500
 const ctx = canvas.getContext('2d')
 
-ctx.fillStyle = 'tomato'
+ctx.fillStyle = get_rand_color()
 ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-const get_random_point = () => ([
-    Math.random() * canvas.width,
-    Math.random() * canvas.height
-])
-
 
 const get_wide_random_point = () => {
     const [x, y] = get_random_point()
@@ -22,11 +47,16 @@ const get_wide_random_point = () => {
         -canvas.height / 13.3 + y * 1.15
     ]
 }
+
+const bg_thickness = 4000 + Math.random() * 6000
+const fore_thickness = 10 + Math.random() * 90
+
+
 ctx.strokeStyle = 'rgb(30, 30, 30)'
 ctx.lineWidth = 1
 ctx.beginPath()
 ctx.moveTo(...get_wide_random_point())
-for (let i = 0; i <= 8000; i++) {
+for (let i = 0; i <= bg_thickness; i++) {
     ctx.lineTo(...get_wide_random_point())
 }
 ctx.stroke()
@@ -40,17 +70,18 @@ const get_narrow_random_point = () => {
     ]
 }
 
+
 ctx.beginPath()
-ctx.strokeStyle = 'green'
-ctx.lineWidth = 2
+ctx.strokeStyle = get_rand_color()
+ctx.lineWidth = 1 + Math.random() * 9
 ctx.moveTo(...get_narrow_random_point())
-for (let i=0; i<100; i++) {
+for (let i=0; i<fore_thickness; i++) {
     ctx.lineTo(...get_narrow_random_point())
 }
 ctx.stroke()
 
 
 
-ctx.strokeStyle = '#193d5f'
+ctx.strokeStyle = get_rand_color()
 ctx.lineWidth = 200
 ctx.strokeRect(0, 0, canvas.width, canvas.height)
