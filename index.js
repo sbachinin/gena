926a4e941a0e7f1fdab8ebf0d2b9750aeb446df4
Sergey Bachinin <sbachinin@yandex.ru>
2023-01-30 13:27:31 +0530

    redraw on click

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const w = 1300,
+    h = 1700,
+    border_width = 120
+
 // UTILS
 
 const get_random_point = () => ([
-    Math.random() * canvas.width,
-    Math.random() * canvas.height
+    Math.random() * w,
+    Math.random() * h
 ])
 
 const get_rand_color = (min = 0, max = 255) => {
@@ -16,6 +20,16 @@ const get_rand_color = (min = 0, max = 255) => {
 }
 
 //////////////////////////////////////////////////////
+// INTERACTION
+
+document.body
+    .addEventListener('click', e => {
+        if (e.target === document.querySelector('canvas.current')) {
+            e.target.classList.remove('current')
+            draw_next_canvas()
+            setTimeout(() => e.target.remove(), 500)
+        }
+    })
 
 
 
@@ -27,63 +41,66 @@ const get_rand_color = (min = 0, max = 255) => {
 
 
 
+////////////////////////////////////////////////////////
+
+const draw_next_canvas = () => {
+    const canvas = document.createElement('canvas')
+    canvas.classList.add('current')
+
+    canvas.width = w
+    canvas.height = h
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = get_rand_color()
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    const get_wide_random_point = () => {
+        const [x, y] = get_random_point()
+        return [
+            -canvas.width / 13.3 + x * 1.15,
+            -canvas.height / 13.3 + y * 1.15
+        ]
+    }
+
+    const bg_thickness = 4000 + Math.random() * 6000
+    const fore_thickness = 10 + Math.random() * 90
 
 
-const w = 1300, h = 1700
+    ctx.strokeStyle = 'rgb(30, 30, 30)'
+    ctx.lineWidth = 1
+    ctx.beginPath()
+    ctx.moveTo(...get_wide_random_point())
+    for (let i = 0; i <= bg_thickness; i++) {
+        ctx.lineTo(...get_wide_random_point())
+    }
+    ctx.stroke()
 
-const border_width = 120
+
+    const get_narrow_random_point = () => {
+        const [x, y] = get_random_point()
+        return [
+            canvas.width / 5 + x * 0.6,
+            canvas.height / 5 + y * 0.6
+        ]
+    }
 
 
-const canvas = document.querySelector('canvas')
-canvas.width = w
-canvas.height = h
-const ctx = canvas.getContext('2d')
+    ctx.beginPath()
+    ctx.strokeStyle = get_rand_color()
+    ctx.lineWidth = 1 + Math.random() * 9
+    ctx.moveTo(...get_narrow_random_point())
+    for (let i = 0; i < fore_thickness; i++) {
+        ctx.lineTo(...get_narrow_random_point())
+    }
+    ctx.stroke()
 
-ctx.fillStyle = get_rand_color()
-ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-const get_wide_random_point = () => {
-    const [x, y] = get_random_point()
-    return [
-        -canvas.width / 13.3 + x * 1.15,
-        -canvas.height / 13.3 + y * 1.15
-    ]
+
+    ctx.strokeStyle = get_rand_color()
+    ctx.lineWidth = border_width
+    ctx.strokeRect(0, 0, canvas.width, canvas.height)
+
+    document.body.prepend(canvas)
 }
 
-const bg_thickness = 4000 + Math.random() * 6000
-const fore_thickness = 10 + Math.random() * 90
-
-
-ctx.strokeStyle = 'rgb(30, 30, 30)'
-ctx.lineWidth = 1
-ctx.beginPath()
-ctx.moveTo(...get_wide_random_point())
-for (let i = 0; i <= bg_thickness; i++) {
-    ctx.lineTo(...get_wide_random_point())
-}
-ctx.stroke()
-
-
-const get_narrow_random_point = () => {
-    const [x, y] = get_random_point()
-    return [
-        canvas.width / 5 + x * 0.6,
-        canvas.height / 5 + y * 0.6
-    ]
-}
-
-
-ctx.beginPath()
-ctx.strokeStyle = get_rand_color()
-ctx.lineWidth = 1 + Math.random() * 9
-ctx.moveTo(...get_narrow_random_point())
-for (let i=0; i<fore_thickness; i++) {
-    ctx.lineTo(...get_narrow_random_point())
-}
-ctx.stroke()
-
-
-
-ctx.strokeStyle = get_rand_color()
-ctx.lineWidth = border_width
-ctx.strokeRect(0, 0, canvas.width, canvas.height)
+draw_next_canvas()
