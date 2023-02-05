ab2bf1c1a17d8bded525e351def98f46246252f7
Sergey Bachinin <sbachinin@yandex.ru>
2023-02-05 11:16:48 +0530

    improve 8

--- 8/index.mjs
+++ 8/index.mjs
@@ -3,47 +3,6 @@ import { controller } from '../controller.mjs'
 const w = 1300,
     h = 1700
 
-const get_random_point = () => ([
-    Math.random() * w,
-    Math.random() * h
-])
-
-// providing narrow range btw min and max leads to more greyness
-const get_rand_color = (min = 0, max = 255, greyness_ratio = 0 /* 0-1 */) => {
-
-    // apply greyness
-    let random1 = Math.random(),
-        random2 = Math.random(),
-        random3 = Math.random()
-    const avg = (random1 + random2 + random3) / 3
-    random1 = random1 - (random1 - avg) * greyness_ratio
-    random2 = random2 - (random2 - avg) * greyness_ratio
-    random3 = random3 - (random3 - avg) * greyness_ratio
-
-    return 'rgb('
-        + (min + random1 * (max - min))
-        + ','
-        + (min + random2 * (max - min))
-        + ','
-        + (min + random3 * (max - min))
-        + ')'
-}
-
-const get_jolly_color = (brightness_ratio = 0.6) => {
-    let random1, random2, random3, avg = 0
-
-    // ensure high avg numbers
-    while (avg < brightness_ratio) {
-        random1 = Math.random()
-        random2 = Math.random()
-        random3 = Math.random()
-        avg = (random1 + random2 + random3) / 3
-    }
-
-    return `rgb(${random1 * 255}, ${random2 * 255}, ${random3 * 255})`
-}
-
-
 const get_random_arr_index = (max) => {
     return Math.floor(Math.random() * max)
 }
@@ -52,33 +11,33 @@ const random_of_arr = (arr) => {
 }
 
 
-const draw_circle = (ctx, tile_x, tile_y, tile_w, tile_h) => {
+const draw_circle = (ctx, tile_x, tile_y, tile_w, tile_h, size_ratio) => {
+    size_ratio *= size_ratio
     ctx.fillStyle = 'white'
     ctx.beginPath()
     const rad = Math.min(tile_w, tile_h) / 2
-    const reduction = 4 + Math.random() * (rad / 3)
     ctx.arc(
         tile_x + tile_w / 2,
         tile_y + tile_h / 2,
-        rad - reduction,
+        rad * size_ratio,
         0,
         360
     )
     ctx.fill()
     ctx.beginPath()
     ctx.strokeStyle = 'white'
-    ctx.lineWidth = 10
+    ctx.lineWidth = 6
     ctx.moveTo(tile_x + tile_w / 2, tile_y + tile_h)
     ctx.lineTo(tile_x + tile_w / 2, tile_y + tile_h / 2)
     ctx.stroke()
 }
 
-const draw_triangle = (ctx, tile_x, tile_y, tile_w, tile_h) => {
+const draw_triangle = (ctx, tile_x, tile_y, tile_w, tile_h, size_ratio) => {
     ctx.fillStyle = 'white'
     ctx.beginPath()
 
-    let size = Math.min(tile_w, tile_h)
-    // size -= Math.random() * (size / 6)
+    let size = Math.min(tile_w, tile_h) * size_ratio
+    if (size < 55) return
 
     const dir = Math.round(Math.random() * 4)
 
@@ -106,7 +65,7 @@ const draw_triangle = (ctx, tile_x, tile_y, tile_w, tile_h) => {
 const draw_line = (ctx, tile_x, tile_y, tile_w, tile_h) => {
     ctx.beginPath()
     ctx.strokeStyle = 'white'
-    ctx.lineWidth = 10
+    ctx.lineWidth = 6
     if (Math.random() > 0.5) {
         ctx.moveTo(tile_x + tile_w / 2, tile_y + tile_h)
         ctx.lineTo(tile_x + tile_w / 2, tile_y)
@@ -127,39 +86,6 @@ const actions = [
     function none() { }
 ]
 
-const colors = [
-    '#D9AF99',
-    '#D1C0A3',
-    '#D9CBBC',
-    '#EBC3BA',
-    '#DAB434',
-
-    '#355a87',
-    '#165399',
-    '#343431',
-    '#343431',
-    '#A96B50',
-    '#A12018',
-    '#A12018',
-    '#F7D867',
-    '#422B1B',
-    '#C88B16',
-    '#C88B16',
-    '#C88B16',
-    '#9E4728',
-    '#9E4728',
-    '#917258',
-    '#BD5637',
-    '#81725E',
-    '#686B5A',
-    '#4383cf',
-    'black',
-    'black',
-    'black',
-    'black',
-    'black',
-    'black',
-]
 
 const klee_colors = [
     '#EA4916',
@@ -179,7 +105,6 @@ const klee_colors = [
     '#6A1A25',
     '#CBBAA6',
     '#11121B',
-    '#BC596D',
     '#12342D',
     '#262537',
     '#262537'
@@ -192,7 +117,7 @@ const draw = (canvas) => {
     canvas.height = h
     const ctx = canvas.getContext('2d')
 
-    const x_count = 6 + Math.round(Math.random() * 4)
+    const x_count = 6 + Math.round(Math.random() * 6)
     const tile_width = w / x_count
     const max_y_count = Math.floor(h / tile_width)
     const min_y_count = Math.floor(h / (tile_width * 1.5))
@@ -205,7 +130,6 @@ const draw = (canvas) => {
 
     for (let xi = 0; xi < x_count; xi++) {
         for (let yi = 0; yi < y_count; yi++) {
-            console.log(JSON.stringify(tiles, null, 2))
             const color = random_of_arr(klee_colors)
             ctx.fillStyle = color
 
@@ -237,7 +161,11 @@ const draw = (canvas) => {
             const is_on_edge = xi === 0 || xi === x_count-1 || yi === 0 || yi === y_count-1
 
             if (action === draw_circle) {
-                if (is_on_edge || get_neighbor_tiles(xi, yi).find(t => t && t.shape)) {
+                if (
+                    is_on_edge
+                    || Math.random() > 0.5
+                    || get_neighbor_tiles(xi, yi).find(t => t && t.shape)
+                ) {
                     // forbid neighbor circles
                     continue
                 } else {
@@ -246,10 +174,7 @@ const draw = (canvas) => {
             }
 
             if (action === draw_triangle) {
-                if (
-                    is_on_edge
-                    || (Math.random() > 0.3 && get_neighbor_tiles(xi, yi).find(t => t && t.shape))
-                ) {
+                if (get_neighbor_tiles(xi, yi).find(t => t && t.shape)) {
                     // forbid neighbor triangles
                     continue
                 } else {
@@ -258,12 +183,23 @@ const draw = (canvas) => {
             }
 
 
+            const center = [x_count / 2, y_count / 2]
+            const x_tiles_from_center = Math.abs(center[0] - (xi + 0.5))
+            const y_tiles_from_center = Math.abs(center[1] - (yi + 0.5))
+            const x_ratio_from_center = x_tiles_from_center / ((x_count - 1) / 2)
+            const y_ratio_from_center = y_tiles_from_center / ((y_count - 1) / 2)
+
+            const centerness_ratio = 1 - (
+                (x_ratio_from_center + y_ratio_from_center) / 2
+            )
+            const size_ratio = centerness_ratio + (1 - centerness_ratio) / 3
             action(
                 ctx,
                 tile_x,
                 tile_y,
                 tile_w,
-                tile_h
+                tile_h,
+                size_ratio
             )
         }
     }
