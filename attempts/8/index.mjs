66fae84faebe5e8676e6cab848bd831e23bd3b02
Sergey Bachinin <sbachinin@yandex.ru>
2023-02-06 14:09:03 +0530

    refac 8

--- attempts/8/index.mjs
+++ attempts/8/index.mjs
@@ -134,6 +134,7 @@ const get_luma = c => {
     return 0.2126 * r + 0.7152 * g + 0.0722 * b // per ITU-R BT.709
 }
 
+
 ////////////////////////////////////////////////////////
 const draw = (canvas) => {
 
@@ -142,159 +143,193 @@ const draw = (canvas) => {
     const ctx = canvas.getContext('2d')
 
     ctx.fillStyle = '#fff'
-    ctx.fillRect(
-        0, 0, w, h
-    )
-
+    ctx.fillRect(0, 0, w, h)
 
     let tile_gap = 0
     if (Math.random() > 0.7) {
-        tile_gap = 6 + Math.random() * 6
+        tile_gap = 20 + Math.random() * 20
     }
 
     const x_count = 5 + Math.round(Math.random() * 4)
-    const tile_width = (w - tile_gap) / x_count
-    const max_y_count = Math.floor(h / tile_width)
-    const min_y_count = Math.floor(h / (tile_width * 1.5))
+    const w_per_slot = (w - tile_gap) / x_count
+    const max_y_count = Math.floor(h / w_per_slot)
+    const min_y_count = Math.floor(h / (w_per_slot * 1.5))
     const y_count = min_y_count + Math.round(Math.random() * (max_y_count - min_y_count))
 
+    const h_per_slot = (h - tile_gap) / y_count
+
+    const slots = Array.from(Array(x_count * y_count)).map(_ => null)
     const tiles = []
 
-    for (let xi = 0; xi < x_count; xi++) {
-        for (let yi = 0; yi < y_count; yi++) {
-            let color = random_of_arr(klee_colors)
-            tiles[yi] = tiles[yi] || []
-            tiles[yi][xi] = {
-                color,
-                shape_size_ratio: 1,
-                x: tile_gap / 2 + (w - tile_gap) / x_count * xi + tile_gap / 2,
-                y: tile_gap / 2 + (h - tile_gap) / y_count * yi + tile_gap / 2,
-                w: (w - tile_gap) / x_count - tile_gap,
-                h: (h - tile_gap) / y_count - tile_gap
-            }
-        }
+
+    const get_tile_rect = (slots_of_tile) => {
+        const xi = Math.min(slots_of_tile) % x_count
+        const yi = Math.floor(Math.min(slots_of_tile) / x_count)
+
+        const x = tile_gap / 2 + w_per_slot * xi
+        const y = tile_gap / 2 + h_per_slot * yi
+        const w = w_per_slot * slots_of_tile.length
+        const h = h_per_slot * slots_of_tile.length
+
+        return [x, y, w, h]
     }
 
+    while (true) {
+        const first_free_slot_index = slots.findIndex(s => s === null)
+        if (first_free_slot_index === -1) {
+            break
+        }
+        const tile = {
+            color: random_of_arr(klee_colors),
+            slots: [first_free_slot_index],
+        }
+        tiles.push(tile)
+        slots[first_free_slot_index] = tiles.length - 1
 
-    for (let xi = 0; xi < x_count; xi++) {
-        for (let yi = 0; yi < y_count; yi++) {
+        const tile_rect = get_tile_rect(tile.slots)
 
-            if (tiles[yi][xi] === undefined) continue
+        ctx.fillStyle = tile.color
+        ctx.fillRect(...tile_rect)
 
-            if (get_luma(tiles[yi][xi].color) > 120) continue // don't draw shapes over light tiles
+        // draw a border to prevent microgaps on the edges
+        ctx.lineWidth = 1
+        ctx.strokeStyle = tile.color
+        ctx.strokeRect(...tile_rect)
 
-            const shape = random_of_arr(Object.keys(shapes))
+        if (tile_gap) {
+            // draw a thicker border; otherwise there is a small gap btw triangle's white and tile_gap' white
+            ctx.lineWidth = tile_gap / 2
+            ctx.strokeStyle = '#fff'
+            ctx.strokeRect(...tile_rect)
+        }
 
-            const is_on_edge = xi === 0 || xi === x_count - 1 || yi === 0 || yi === y_count - 1
 
-            const get_neighbor_tiles = (x_i, y_i) => ([
-                tiles[y_i - 1]?.[x_i - 1],
-                tiles[y_i - 1]?.[x_i],
-                tiles[y_i - 1]?.[x_i + 1],
-                tiles[y_i][x_i - 1],
-                tiles[y_i][x_i + 1],
-                tiles[y_i + 1]?.[x_i - 1],
-                tiles[y_i + 1]?.[x_i],
-                tiles[y_i + 1]?.[x_i + 1],
-            ])
+        // check in which directions can extend
+        // if none, try make another tile
 
-            if (shape === 'circle') {
-                if (
-                    is_on_edge
-                    || (
-                        // in 75% cases forbid drawing circles next to other shapes
-                        Math.random() > 0.25
-                        && get_neighbor_tiles(xi, yi).find(t => t && t.shape)
-                    )
-                ) {
-                    continue
-                } else {
-                    tiles[yi][xi].shape = 'circle'
-                    if (Math.random() > 0.5) { // paint n neighbors in random direction in same color
-                        // TODO need to check if there's space to grow on the right or bottom
-                        if (Math.random() > 0.5) {
-                            // extend right
-                            tiles[yi][xi].w = tiles[yi][xi].w * 2 + tile_gap
-                            tiles[yi][xi + 1] = undefined
-                        } else {
-                            // extend down
-                            tiles[yi][xi].h = tiles[yi][xi].h * 2 + tile_gap
-                            tiles[yi + 1][xi] = undefined
-                        }
-                        // extending "backwards" (top & left is forbidden for it caused minor glitches like eating the neighbor which was extended too)
+        /* 
+                if (Math.random() > 0.5) { // paint n neighbors in random direction in same color
+                    // TODO need to check if there's space to grow on the right or bottom
+                    if (Math.random() > 0.5) {
+                        // extend right
+                        tile.w = tile.w * 2 + tile_gap
+                    } else {
+                        // extend down
+                        tile.h = tile.h * 2 + tile_gap
                     }
-                }
-            }
+                    // extending "backwards" (top & left is forbidden for it caused minor glitches like eating the neighbor which was extended too)
+                } */
 
-            if (shape === 'triangle') {
-                if (get_neighbor_tiles(xi, yi).find(t => t && t.shape)) {
-                    // forbid neighbor triangles
-                    continue
-                } else {
-                    tiles[yi][xi].shape = 'triangle'
-                }
-            }
-
-
-            const center = [x_count / 2, y_count / 2]
-            const x_tiles_from_center = Math.abs(center[0] - (xi + 0.5))
-            const y_tiles_from_center = Math.abs(center[1] - (yi + 0.5))
-            const x_ratio_from_center = x_tiles_from_center / ((x_count - 1) / 2)
-            const y_ratio_from_center = y_tiles_from_center / ((y_count - 1) / 2)
-
-            const centerness_ratio = 1 - (
-                (x_ratio_from_center + y_ratio_from_center) / 2
-            )
-            const size_ratio = centerness_ratio + (1 - centerness_ratio) / 3
-            tiles[yi][xi].shape_size_ratio = size_ratio
-        }
     }
 
 
+    // for (let xi = 0; xi < x_count; xi++) {
+    //     for (let yi = 0; yi < y_count; yi++) {
+
+    //         if (tiles[yi][xi] === undefined) continue
+
+    //         if (get_luma(tiles[yi][xi].color) > 120) continue // don't draw shapes over light tiles
+
+    //         const shape = random_of_arr(Object.keys(shapes))
+
+    //         const is_on_edge = xi === 0 || xi === x_count - 1 || yi === 0 || yi === y_count - 1
+
+    //         const get_neighbor_tiles = (x_i, y_i) => ([
+    //             tiles[y_i - 1]?.[x_i - 1],
+    //             tiles[y_i - 1]?.[x_i],
+    //             tiles[y_i - 1]?.[x_i + 1],
+    //             tiles[y_i][x_i - 1],
+    //             tiles[y_i][x_i + 1],
+    //             tiles[y_i + 1]?.[x_i - 1],
+    //             tiles[y_i + 1]?.[x_i],
+    //             tiles[y_i + 1]?.[x_i + 1],
+    //         ])
+
+    //         if (shape === 'circle') {
+    //             if (
+    //                 is_on_edge
+    //                 || (
+    //                     // in 75% cases forbid drawing circles next to other shapes
+    //                     Math.random() > 0.25
+    //                     && get_neighbor_tiles(xi, yi).find(t => t && t.shape)
+    //                 )
+    //             ) {
+    //                 continue
+    //             } else {
+    //                 tiles[yi][xi].shape = 'circle'
+
+    //             }
+    //         }
+
+    //         if (shape === 'triangle') {
+    //             if (get_neighbor_tiles(xi, yi).find(t => t && t.shape)) {
+    //                 // forbid neighbor triangles
+    //                 continue
+    //             } else {
+    //                 tiles[yi][xi].shape = 'triangle'
+    //             }
+    //         }
 
 
-    tiles.forEach((row, yi) => {
-        row.forEach((tile, xi) => {
+    //         const center = [x_count / 2, y_count / 2]
+    //         const x_tiles_from_center = Math.abs(center[0] - (xi + 0.5))
+    //         const y_tiles_from_center = Math.abs(center[1] - (yi + 0.5))
+    //         const x_ratio_from_center = x_tiles_from_center / ((x_count - 1) / 2)
+    //         const y_ratio_from_center = y_tiles_from_center / ((y_count - 1) / 2)
 
-            if (tile === undefined) return
-
-            ctx.fillStyle = tile.color
-            ctx.fillRect(
-                tile.x,
-                tile.y,
-                tile.w,
-                tile.h
-            )
-            // draw a border to prevent microgaps on the edges
-            ctx.lineWidth = 1
-            ctx.strokeStyle = tile.color
-            ctx.strokeRect(tile.x,
-                tile.y,
-                tile.w,
-                tile.h)
+    //         const centerness_ratio = 1 - (
+    //             (x_ratio_from_center + y_ratio_from_center) / 2
+    //         )
+    //         const size_ratio = centerness_ratio + (1 - centerness_ratio) / 3
+    //         tiles[yi][xi].shape_size_ratio = size_ratio
+    //     }
+    // }
 
 
-            if (tile.shape) {
-                shapes[tile.shape](
-                    xi,
-                    yi,
-                    tiles,
-                    ctx
-                )
-            }
 
 
-            if (tile_gap) {
-                // draw a thicker border; otherwise there is a small gap btw triangle's white and tile_gap' white
-                ctx.lineWidth = 4
-                ctx.strokeStyle = '#fff'
-                ctx.strokeRect(tile.x,
-                    tile.y,
-                    tile.w,
-                    tile.h)
-            }
-        })
-    })
+    // tiles.forEach((row, yi) => {
+    //     row.forEach((tile, xi) => {
+
+    //         if (tile === undefined) return
+
+    //         ctx.fillStyle = tile.color
+    //         ctx.fillRect(
+    //             tile.x,
+    //             tile.y,
+    //             tile.w,
+    //             tile.h
+    //         )
+    //         // draw a border to prevent microgaps on the edges
+    //         ctx.lineWidth = 1
+    //         ctx.strokeStyle = tile.color
+    //         ctx.strokeRect(tile.x,
+    //             tile.y,
+    //             tile.w,
+    //             tile.h)
+
+
+    //         if (tile.shape) {
+    //             shapes[tile.shape](
+    //                 xi,
+    //                 yi,
+    //                 tiles,
+    //                 ctx
+    //             )
+    //         }
+
+
+    //         if (tile_gap) {
+    //             // draw a thicker border; otherwise there is a small gap btw triangle's white and tile_gap' white
+    //             ctx.lineWidth = 4
+    //             ctx.strokeStyle = '#fff'
+    //             ctx.strokeRect(tile.x,
+    //                 tile.y,
+    //                 tile.w,
+    //                 tile.h)
+    //         }
+    //     })
+    // })
 
 
 
