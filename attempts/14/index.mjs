8adad9543a91a7945bcd098cd5f51edf359d2b29
Sergey Bachinin <sbachinin@yandex.ru>
2023-03-28 10:27:22 +0530

    add 14 (circles 4)

---
+++ attempts/14/index.mjs
@@ -0,0 +1,107 @@
+import { controller } from '../../controller.mjs'
+
+const w = 1300,
+    h = 1700
+
+const get_random_arr_index = (max) => {
+    return Math.floor(Math.random() * max)
+}
+const random_of_arr = (arr) => {
+    return arr[get_random_arr_index(arr.length)]
+}
+
+
+const klee_colors = [
+    '#EA4916',
+    '#1E6A83',
+    '#4C6666',
+    '#356D75',
+    '#1A6980',
+    '#55655B',
+    '#55655B',
+    '#1F6B80',
+    '#347F9C',
+    '#25718D',
+    '#C88334',
+    '#EED76B',
+    '#EAA27F',
+    '#D49F88',
+    '#6A1A25',
+    '#CBBAA6',
+    '#11121B',
+    '#12342D',
+    '#262537',
+    '#262537'
+]
+
+const get_luma = c => {
+    var c = c.substring(1)      // strip #
+    var rgb = parseInt(c, 16)   // convert rrggbb to decimal
+    var r = (rgb >> 16) & 0xff  // extract red
+    var g = (rgb >> 8) & 0xff  // extract green
+    var b = (rgb >> 0) & 0xff  // extract blue
+
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b // per ITU-R BT.709
+}
+
+const get_centerness_ratio = (x_count, y_count, xi, yi) => {
+    const center = [x_count / 2, y_count / 2]
+    const x_tiles_from_center = Math.abs(center[0] - (xi + 0.5))
+    const y_tiles_from_center = Math.abs(center[1] - (yi + 0.5))
+    const x_ratio_from_center = x_tiles_from_center / ((x_count - 1) / 2)
+    const y_ratio_from_center = y_tiles_from_center / ((y_count - 1) / 2)
+
+    return 1 - (
+        (x_ratio_from_center + y_ratio_from_center) / 2
+    )
+}
+
+const ring_width = 20
+
+const draw_circle = (ctx) => {
+    
+    const center = [
+        Math.random() * w,
+        Math.random() * h
+    ]
+    let rad = 80 + Math.random() * 80
+
+    ctx.fill()
+
+    while (rad > 0) {
+        ctx.beginPath()
+        ctx.arc(
+            center[0],
+            center[1],
+            rad,
+            0,
+            360
+        )
+        ctx.stroke()
+        rad -= ring_width
+    }
+}
+
+////////////////////////////////////////////////////////
+const draw = (canvas) => {
+
+    canvas.width = w
+    canvas.height = h
+    const ctx = canvas.getContext('2d')
+    ctx.fillStyle = `hsl(0 0% 30%)`
+    ctx.fillRect(0, 0, w, h)
+
+    ctx.lineWidth = 5
+
+    const circ_count = 150
+
+    for (let x = 0; x < circ_count; x++) {
+        const ratio = x / (circ_count / 100) // % btw 0 and circ_count
+        const l = 30 + ratio * 0.7
+        ctx.fillStyle = `hsl(0 ${l/2}% ${l}%)`
+        ctx.strokeStyle = `hsl(0 ${ratio}% 50%)`
+        draw_circle(ctx)
+    }
+}
+
+controller.init(draw)
